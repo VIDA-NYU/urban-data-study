23202e5673d965bb75d85b9293fa8147990f2aac
Kien Pham <kienpham@gray02.poly.edu>
2014-05-16 15:19:41 -0400

    add tagcloud script

---
+++ tagcloud/README.txt
@@ -0,0 +1,8 @@
+Rscript tagcloud.r data/allcities.txt
+Rscript tagcloud.r data/chicago.txt
+Rscript tagcloud.r data/kansas.txt
+Rscript tagcloud.r data/nyc.txt
+Rscript tagcloud.r data/seattle.txt
+Rscript tagcloud.r data/top1000.txt
+Rscript tagcloud.r data/top100.txt
+Rscript tagcloud.r data/top500.txt
--- tagcloud/tagcloud.r
+++ tagcloud/tagcloud.r
@@ -2,10 +2,11 @@ require("tm");
 require("wordcloud");
 input <- commandArgs(trailingOnly = TRUE)
 print(args)
-tags <- Corpus (DirSource(input));
+#tags <- Corpus (DirSource(input));
+tags <- Corpus(VectorSource(readLines(input)))
 tags <- tm_map(tags, stripWhitespace);
 tags <- tm_map(tags, tolower);
 tags <- tm_map(tags, removeWords, stopwords("english"));
 #par(mfrow=c(3,1))
-wordcloud(tags, scale=c(10,0.3), max.words=5000, random.order=FALSE, rot.per=0.35, use.r.layout=TRUE, colors=brewer.pal(8, "Dark2"));
-#wordcloud(tags, scale=c(10,1), random.order=FALSE, rot.per=0.35, use.r.layout=TRUE, colors=brewer.pal(8, "Dark2"));
+#wordcloud(tags, scale=c(10,0.3), max.words=5000, random.order=FALSE, rot.per=0.35, use.r.layout=TRUE, colors=brewer.pal(8, "Dark2"));
+wordcloud(tags, scale=c(10,1), random.order=FALSE, rot.per=0.35, use.r.layout=TRUE, colors=brewer.pal(8, "Dark2"));
