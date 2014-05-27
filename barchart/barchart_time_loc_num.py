e8ee5bd497082a7d08926cb75642cbc7acf85894
Kien Pham <kienpt.vie@gmail.com>
2014-05-27 17:16:38 -0400

    Remove Null Type in the barchart and add Copyright

--- barchart/barchart_time_loc_num.py
+++ barchart/barchart_time_loc_num.py
@@ -1,3 +1,37 @@
+###############################################################################
+##
+## Copyright (C) 2014, New York University.
+## All rights reserved.
+## Contact: kien.pham@nyu.edu
+##
+## This file is part of the VIDA team submission for the SIGMOD 2014 Programming Contest.
+##
+## "Redistribution and use in source and binary forms, with or without 
+## modification, are permitted provided that the following conditions are met:
+##
+##  - Redistributions of source code must retain the above copyright notice, 
+##    this list of conditions and the following disclaimer.
+##  - Redistributions in binary form must reproduce the above copyright 
+##    notice, this list of conditions and the following disclaimer in the 
+##    documentation and/or other materials provided with the distribution.
+##  - Neither the name of New York University nor the names of its 
+##    contributors may be used to endorse or promote products derived from 
+##    this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+## THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+## PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+## EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+## OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+## OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+###############################################################################
+
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
@@ -39,7 +73,7 @@ def getData():
 
 def main(argv):
   s, m = getData()
-  label = ["Time", "Location", "Number", "Null"] #Label for x axis
+  label = ["Location", "Time", "Number"] #Label for x axis
   _color = {  
       "Baltimore":"#a6cee3",
       "Chicago":"#1f78b4",
@@ -56,7 +90,7 @@ def main(argv):
   #for key in m.keys():
   #  plt.plot(axis, m[key], marker=_marker[key], label=key)
   
-  N = 4
+  N = 3
   width = 0.07
   ind = np.arange(N)
   print ind
@@ -70,7 +104,7 @@ def main(argv):
   plt.ylabel('Percentage of Datasets', fontsize=30)
   plt.xticks(ind+0.5, label, fontsize=25)
   plt.tick_params(axis='y', labelsize=17)
-  ax.legend(s, loc=1,prop={'size':13})
+ # ax.legend(s, loc=1,prop={'size':13})
 
   plt.show()
 
