2efc535778407bd7e556d69b1b946a28bc2e9b98
Kien Pham <kienpt.vie@gmail.com>
2014-07-15 15:32:36 -0400

    update script to generate tagcloud: remove file extensions.

--- tagcloud/README.txt
+++ tagcloud/README.txt
@@ -1,10 +1,10 @@
 HOWTORUN:
 
-$Rscript tagcloud.r data/allcities.txt
-$Rscript tagcloud.r data/chicago.txt
+$Rscript tagcloud.r data/allcities.txt 500 0.5 10
+$Rscript tagcloud.r data/chicago.txt 100 1 5
 $Rscript tagcloud.r data/kansas.txt
-$Rscript tagcloud.r data/nyc.txt
-$Rscript tagcloud.r data/seattle.txt
-$Rscript tagcloud.r data/top1000.txt
-$Rscript tagcloud.r data/top100.txt
-$Rscript tagcloud.r data/top500.txt
+$Rscript tagcloud.r data/nyc.txt 500 1 10
+$Rscript tagcloud.r data/seattle.txt 500 1 10
+$Rscript tagcloud.r data/top1000.txt 200 0.3 7
+$Rscript tagcloud.r data/top100.txt 200 0.3 7
+$Rscript tagcloud.r data/top500.txt 200 0.3 7
--- tagcloud/tagcloud.r
+++ tagcloud/tagcloud.r
@@ -1,12 +1,24 @@
 require("tm");
 require("wordcloud");
-input <- commandArgs(trailingOnly = TRUE)
-print(args)
+input <- commandArgs(trailingOnly = TRUE);
+min_scale = 1;
+max_scale = 10;
+max_word = 700;
+if (length(input) != 1)
+{
+  max_word = as.numeric(input[2]);
+  min_scale = as.numeric(input[3]);
+  max_scale = as.numeric(input[4]);
+}
+print(min_scale);
+print(max_word);
 #tags <- Corpus (DirSource(input));
-tags <- Corpus(VectorSource(readLines(input)))
+tags <- Corpus(VectorSource(readLines(input[1])));
 tags <- tm_map(tags, stripWhitespace);
 tags <- tm_map(tags, tolower);
 tags <- tm_map(tags, removeWords, stopwords("english"));
+skipwords <- as.character(readLines("data/skipwords.txt"));
+tags <- tm_map(tags, removeWords, skipwords);
 #par(mfrow=c(3,1))
 #wordcloud(tags, scale=c(10,0.3), max.words=5000, random.order=FALSE, rot.per=0.35, use.r.layout=TRUE, colors=brewer.pal(8, "Dark2"));
-wordcloud(tags, scale=c(10,1), random.order=FALSE, rot.per=0.35, use.r.layout=TRUE, colors=brewer.pal(8, "Dark2"));
+wordcloud(tags, scale=c(max_scale,min_scale), max.words=max_word, random.order=FALSE, rot.per=0.35, use.r.layout=TRUE, colors=brewer.pal(8, "Dark2"));
