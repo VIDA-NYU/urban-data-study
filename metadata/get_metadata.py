fda78fd927a10e51b7ef81067a193b3a7bb785cb
Kien Pham <kienpt.vie@gmail.com>
2014-07-22 13:14:48 -0400

    Retrieve publicationdate, createddate and blob size for line charts

--- metadata/get_metadata.py
+++ metadata/get_metadata.py
@@ -1,35 +1,3 @@
-###############################################################################
-##
-## Copyright (C) 2014, New York University.
-## All rights reserved.
-## Contact: kien.pham@nyu.edu
-##
-## "Redistribution and use in source and binary forms, with or without 
-## modification, are permitted provided that the following conditions are met:
-##
-##  - Redistributions of source code must retain the above copyright notice, 
-##    this list of conditions and the following disclaimer.
-##  - Redistributions in binary form must reproduce the above copyright 
-##    notice, this list of conditions and the following disclaimer in the 
-##    documentation and/or other materials provided with the distribution.
-##  - Neither the name of New York University nor the names of its 
-##    contributors may be used to endorse or promote products derived from 
-##    this software without specific prior written permission.
-##
-## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-## THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-## PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-## EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-## OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-## OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
-## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-##
-###############################################################################
-
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #Input:
@@ -76,6 +44,8 @@ def main(argv):
   id_tag_f = codecs.open(outpath + '/' + city + '_id_tag.csv', 'a', 'utf-8')
   id_downloadcount_f = codecs.open(outpath + '/' + city + '_id_downloadcount.csv', 'a', 'utf-8')
   id_viewcount_f = codecs.open(outpath + '/' + city + '_id_viewcount.csv', 'a', 'utf-8')
+  id_date_f = open(outpath + '/' + city + '_id_date.csv', 'w')
+  id_size_f = open(outpath + '/' + city + '_id_size.csv', 'w')
   
   #Load id of the datasets whose metadatas were already retrieved
   ids = loadIDs(outpath)
@@ -120,6 +90,22 @@ def main(argv):
       else:
         id_downloadcount_f.write(id + "\tnull\n")
 
+      #Publication date
+      pdate = "None"
+      cdate = "None"
+      if js[j].has_key('publicationDate'):
+        pdate = str(js[j]['publicationDate']) #Timestamp format
+      #Created date
+      if js[j].has_key('createdAt'):
+        cdate = str(js[j]['createdAt']) #Timestamp format
+      id_date_f.write(id + "\t" + cdate + "\t" + pdate + "\n")
+
+      #blobsize
+      bsize = "None"
+      if js[j].has_key('blobFileSize'):
+        bsize = str(js[j]['blobFileSize'])
+        id_size_f.write(id + "\t" + bsize + "\n")
+
       #TAGS
       tag  = ''
       if js[j].has_key('tags'):
@@ -150,7 +136,9 @@ def main(argv):
   schema_f.close()
   id_tag_f.close()
   id_downloadcount_f.close()
-  id_viewcount_f.close()  
+  id_viewcount_f.close() 
+  id_date_f.close() 
+  id_size_f.close()
 
 if __name__ == "__main__":
   main(sys.argv[1:])
